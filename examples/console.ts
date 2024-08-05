0707fe3498a65dc15fd197d115999cf1345edc49
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2024-08-05 17:13:05 -0400

    feat: console colors

---
+++ examples/console.ts
@@ -0,0 +1,5 @@
+console.log("This is a log message");
+console.debug("This is a debug message");
+console.warn("This is a warning message");
+console.error("This is an error message");
+console.info("This is an info message");
