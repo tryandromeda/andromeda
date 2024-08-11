40957cbab0e4a6b9aa3b7907fc0ac4014952b692
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2024-08-11 00:02:12 -0400

    feat(runtime): env

---
+++ examples/env.ts
@@ -0,0 +1,4 @@
+console.log(internal_set_env("HI", "Hello, World!"));
+console.log(internal_set_env("HI2", "Hello, World Again!"));
+console.log(internal_delete_env("HI"));
+console.log(internal_get_env("HI"));
