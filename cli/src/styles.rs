a355b5bf5d0433dffbf1a19b47c907b38f60fcc5
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2025-06-04 21:47:36 -0400

    chore: fmt

--- cli/src/styles.rs
+++ cli/src/styles.rs
@@ -2,13 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use clap::builder::{
-    Styles,
-    styling::{AnsiColor, Effects},
-};
+use clap::builder::styling::{AnsiColor, Effects};
 use console::Style;
 
-pub const ANDROMEDA_STYLING: Styles = Styles::styled()
+pub const ANDROMEDA_STYLING: clap::builder::Styles = clap::builder::Styles::styled()
     .header(AnsiColor::BrightCyan.on_default().effects(Effects::BOLD))
     .usage(AnsiColor::BrightCyan.on_default().effects(Effects::BOLD))
     .literal(AnsiColor::Yellow.on_default().effects(Effects::BOLD))
