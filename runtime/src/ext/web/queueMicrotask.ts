07bdb3ae26bfdae4cbd448cb3e5ae76d199447c2
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2025-06-11 01:56:38 -0400

    feat(ext/web): queueMicrotask

---
+++ runtime/src/ext/web/queueMicrotask.ts
@@ -0,0 +1,22 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// deno-lint-ignore-file
+
+function queueMicrotask(callback: () => void): void {
+  if (typeof callback !== "function") {
+    throw new TypeError(
+      "The callback provided as an argument to queueMicrotask must be a function.",
+    );
+  }
+
+  Promise.resolve().then(() => {
+    try {
+      callback();
+    } catch (error) {
+      console.error("Uncaught error in microtask callback:", error);
+      throw error;
+    }
+  });
+}
