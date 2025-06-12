7ad0615878ce8f7e01c69eb54607168d7818973e
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2025-06-11 22:36:46 -0400

    chore: fmt

--- runtime/src/ext/web/mod.rs
+++ runtime/src/ext/web/mod.rs
@@ -6,7 +6,7 @@ use andromeda_core::{Extension, ExtensionOp};
 use nova_vm::{
     ecmascript::{
         builtins::ArgumentsList,
-        execution::{agent::ExceptionType, Agent, JsResult},
+        execution::{Agent, JsResult, agent::ExceptionType},
         types::Value,
     },
     engine::context::{Bindable, GcScope, NoGcScope},
