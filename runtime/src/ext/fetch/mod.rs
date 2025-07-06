4a1ca5b4cb3175e73295a5b7378a1f91b9760bc2
yossydev <yossydev@yossy.dev>
2025-07-06 15:36:01 +0900

    fix: clippy

--- runtime/src/ext/fetch/mod.rs
+++ runtime/src/ext/fetch/mod.rs
@@ -2,12 +2,25 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-mod fetch;
 mod headers;
 mod request;
 mod response;
 
-pub use fetch::*;
+use andromeda_core::Extension;
 pub use headers::*;
 pub use request::*;
 pub use response::*;
+
+#[derive(Default)]
+pub struct FetchExt;
+
+impl FetchExt {
+    pub fn new_extension() -> Extension {
+        Extension {
+            name: "fetch",
+            ops: vec![],
+            storage: None,
+            files: vec![include_str!("./fetch/mod.ts")],
+        }
+    }
+}
