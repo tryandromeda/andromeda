145925b45d244bb128fbee0b875c4466cd66ded5
Jin Wei Tan <48111396+JinWeiTan@users.noreply.github.com>
2025-07-17 23:36:15 +0800

    fix: spelling

--- examples/canvas_gradient.ts
+++ examples/canvas_gradient.ts
@@ -13,18 +13,18 @@ linearGradient.addColorStop(1, "blue")
 ctx.fillStyle = linearGradient
 ctx.fillRect(0, 0, 200, 200)
 
-const radialGradiant1 = ctx.createRadialGradient(250, 100, 50, 300, 100, 100)
-radialGradiant1.addColorStop(0, "red")
-radialGradiant1.addColorStop(1, "blue")
+const radialGradient1 = ctx.createRadialGradient(250, 100, 50, 300, 100, 100)
+radialGradient1.addColorStop(0, "red")
+radialGradient1.addColorStop(1, "blue")
 
-ctx.fillStyle = radialGradiant1
+ctx.fillStyle = radialGradient1
 ctx.fillRect(200, 0, 200, 200)
 
-const radialGradiant2 = ctx.createRadialGradient(100, 300, 50, 100, 300, 100)
-radialGradiant2.addColorStop(0, "red")
-radialGradiant2.addColorStop(1, "blue")
+const radialGradient2 = ctx.createRadialGradient(100, 300, 50, 100, 300, 100)
+radialGradient2.addColorStop(0, "red")
+radialGradient2.addColorStop(1, "blue")
 
-ctx.fillStyle = radialGradiant2
+ctx.fillStyle = radialGradient2
 ctx.fillRect(0, 200, 200, 200)
 
 const conicGradient = ctx.createConicGradient(0, 300, 300)
