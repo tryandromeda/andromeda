72f0018fedc8e7bd7a08ff291b0caf322d1bbd0a
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2025-09-01 23:18:58 -0400

    chore: bump version

--- examples/fetch.ts
+++ examples/fetch.ts
@@ -1,17 +1,12 @@
-// Simple fetch example
-async function getData() {
-  const url = "https://httpbin.org/json";
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const result = await response.text();
-    console.log(result);
-  } catch (error) {
-    console.error(error.message);
+const url = "https://httpbin.org/json";
+try {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
   }
-}
 
-getData();
+  const result = await response.json();
+  console.log(result);
+} catch (error) {
+  console.error(error.message);
+}
